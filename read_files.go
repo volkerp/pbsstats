f565b6dde0dc6e8d096a7572e3fc470db98b88f5
volker <volker@openbios.org>
2025-04-23 09:13:59 +0200

    WIP

--- read_files.go
+++ read_files.go
@@ -8,25 +8,30 @@ import (
 	"sync"
 )
 
-type DigestCounter map[Digest]int
+type FileInfo struct {
+	refChunks    []uint32 // list of referred chunks, index into the digest index
+	uniqueChunks uint32   // number of unique chunks
+}
 
-type RefChunks []uint32
-
-// map from filename to a list of chunks
-type FileRefChunks map[string]RefChunks
+func NewFileInfo() *FileInfo {
+	return &FileInfo{
+		refChunks:    make([]uint32, 0),
+		uniqueChunks: 0,
+	}
+}
 
 type DigestIndexMap struct {
-	index   []Digest       // Slice to maintain order and index access
-	counter map[Digest]int // Map for fast lookup
-	fileref FileRefChunks
+	index    []Digest       // Slice to maintain order and index access
+	counter  map[Digest]int // Map for fast lookup
+	fileInfo map[string]FileInfo
 }
 
 // NewDigestIndexMap creates and initializes a new DigestIndexMap
 func NewDigestIndexMap() *DigestIndexMap {
 	return &DigestIndexMap{
-		index:   make([]Digest, 0),
-		counter: make(map[Digest]int),
-		fileref: make(FileRefChunks),
+		index:    make([]Digest, 0),
+		counter:  make(map[Digest]int),
+		fileInfo: make(map[string]FileInfo),
 	}
 }
 
@@ -55,10 +60,22 @@ func (d *DigestIndexMap) addFileRef(filename string, digest Digest) {
 		fmt.Printf("Digest %x not found in index\n", digest)
 		return
 	}
-	if _, exists := d.fileref[filename]; !exists {
-		d.fileref[filename] = make(RefChunks, 0)
+	if _, exists := d.fileInfo[filename]; !exists {
+		d.fileInfo[filename] = *NewFileInfo()
 	}
-	d.fileref[filename] = append(d.fileref[filename], uint32(digestIndex))
+	fileInfo := d.fileInfo[filename]
+	fileInfo.refChunks = append(fileInfo.refChunks, uint32(digestIndex))
+	d.fileInfo[filename] = fileInfo
+}
+
+func calculateFileDedup(digestsIndex *DigestIndexMap) int {
+	dedupCount := 0
+	for _, count := range digestsIndex.counter {
+		if count > 1 {
+			dedupCount += count - 1
+		}
+	}
+	return dedupCount
 }
 
 func scanAndCountDigests(root string) (*DigestIndexMap, error) {
@@ -139,9 +156,9 @@ func printOccurrences(digestIndex *DigestIndexMap, topN int) {
 
 func printDigestRefs(digestsIndex *DigestIndexMap) {
 	fmt.Println("File references for each digest:")
-	for filename, refs := range digestsIndex.fileref {
+	for filename, fileInfo := range digestsIndex.fileInfo {
 		fmt.Printf("%s: ", filename)
-		for _, ref := range refs {
+		for _, ref := range fileInfo.refChunks {
 			fmt.Printf("%d ", ref)
 		}
 		fmt.Println()
